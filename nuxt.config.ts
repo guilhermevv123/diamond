de1e36a349db7338854223ceb3724da4254d0d91
guilhermevv123 <guilhermevvui1227@gmail.com>
2026-02-06 21:52:50 -0300

    chore: remove debug files after successful deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,15 +4,17 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase'],
   supabase: {
-    redirect: false,
-    url: process.env.SUPABASE_URL,
-    key: process.env.SUPABASE_KEY
+    redirect: false
   },
   css: ['~/assets/css/main.css'],
   runtimeConfig: {
     pusherAppId: process.env.PUSHER_APP_ID,
     pusherSecret: process.env.PUSHER_SECRET,
     public: {
+      supabase: {
+        url: process.env.SUPABASE_URL || '',
+        key: process.env.SUPABASE_KEY || '',
+      },
       pusherKey: process.env.PUSHER_KEY,
       pusherCluster: process.env.PUSHER_CLUSTER,
     }
